cc2717a1df7ae2d7e9a10d5b04669fd4fc95c859
mrmoustach <bossissamos@gmail.com>
2021-09-25 18:08:25 +0100

    export kinda done, needs writing to vars

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 13:13:02 by iharchi           #+#    #+#             */
-/*   Updated: 2021/09/25 14:46:25 by iharchi          ###   ########.fr       */
+/*   Updated: 2021/09/25 18:07:02 by iharchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static char	**split_equals(char *str)
 	if (str[i + 1])
 		tab[1] = ft_substr(str, i + 1, ft_strlen(str) - i);
 	else
-		tab[1] = NULL;
+		tab[1] = ft_strdup("");
 	tab[2] = NULL;
 	return (tab);
 }
@@ -41,28 +41,16 @@ static char	*var_line(char *line)
 	int		count;
 	int		i;
 	//TODO : free this split
-	// tab = ft_split(line, '=');
-	// count = table_count(tab);
-	// if (count <= 1)
-	// {
-	// 	ret = ft_strdup(line);
-	// 	ret = ft_strjoin(ret, ft_strdup("\"\""));
-	// 	return (ret);
-	// }
-	// ret = ft_strdup(tab[0]);
-	// ret = ft_strjoin(ret, ft_strdup("=\""));
-	// i = 1;
-	// while (tab[i])
-	// {
-			
-	// 	ret = ft_strjoin(ret, ft_strdup(tab[i++]));
-	// }
-	// ret = ft_strjoin(ret, ft_strdup("\""));
 	tab = split_equals(line);
-	i = 0;
-	while (tab[i]){
-		printf("%s\n", tab[i++]);
+	if (ft_strchr(line, '='))
+	{
+		ret = ft_strjoin(tab[0], "=\"");
+		ret = ft_strjoin(ret, tab[1]);
+		ret = ft_strjoin(ret, "\"");
 	}
+	else
+		ret = ft_strdup(line);
+	printf("%s\n", ret);
 	return (ret);
 }
 
@@ -78,12 +66,8 @@ void	builtin_export(t_token command)
 		i = 0;
 		while (command.args[i])
 		{
-			if (ft_strchr(command.args[i], '='))
-			{
-				printf("%s\n", var_line(command.args[i]));
-			}
-			else
-				printf("%s\n", command.args[i]);
+			var_line(command.args[i]);
+			// printf("%s\n", var_line(command.args[i]));
 			i++;
 		}
 	}
