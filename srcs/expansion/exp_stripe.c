2085246f12b85188d4787c66ed03c20ae30361da
Oumar Mimouni <omimouni@student.1337.ma>
2021-09-28 17:32:37 +0100

    quotation stripe is done

--- srcs/expansion/exp_stripe.c
+++ srcs/expansion/exp_stripe.c
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 08:04:44 by omimouni          #+#    #+#             */
-/*   Updated: 2021/09/28 16:50:38 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/09/28 17:30:59 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,27 @@ char
 	int		i;
 	int		j;
 	int		con;
+	int		end_con;
 
 	tmp = malloc(sizeof(char) * (*length + 1));
+	ft_memset(tmp, 'x', *length);
 	i = 0;
 	j = 0;
 	con = 0;
 	while (str[i])
 	{
 		con = exp_create_context(str[i], con);
-		if (str[i] != '\'' || str[i] != '"'){
-			printf("%c in quotes %d \n", str[i], con);
+		end_con = con;
+		end_con = exp_create_context(str[i], con);
+		if (con && end_con)
+		{
+			printf("%c in quote %d %d\n", str[i], con, end_con);
 			tmp[j] = str[i];
 			j++;
 		}
 		i++;
 	}
-	tmp[j] = '\0';
+	tmp[*length] = '\0';
+	printf("\nstripped sentence %s\n", tmp);
 	return (tmp);
 }
