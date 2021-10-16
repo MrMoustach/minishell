8e976590799c11f372f82e785a09037acfb7d068
Oumar Mimouni <omimouni@student.1337.ma>
2021-10-16 16:13:56 +0100

    norming parser and testing

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -3,150 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 16:31:46 by zed               #+#    #+#             */
-/*   Updated: 2021/10/15 17:39:57 by iharchi          ###   ########.fr       */
+/*   Updated: 2021/10/16 16:13:34 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// TODO : cleanup priority N1
-
-// t_list	*parser(char	*line)
-// {
-// 	t_spliter	split;
-// 	t_syntax	syntax;
-// 	t_parser	parser;
-// 	t_list		*tmp;
-// 	t_list		*before;
-// 	t_token		*tmp_token;
-// 	t_list		*tmp_lst;
-// 	t_list		*last_assignable_token;
-
-// 	split = spliter(line);
-// 	tokenizer(split.tokens);
-// 	syntax = syntax_analysis(split.tokens);
-// 	if (syntax.error)
-// 	{
-// 		printf("syntax error m8 code : %d near %s\n", syntax.error, syntax.err_token.str);
-// 		g_shell.error = 2;
-// 		return (split.tokens);
-// 	}
-// 	tmp = split.tokens;
-// 	before = NULL;
-// 	parser.context = 0;
-// 	parser.last_command = NULL;
-// 	last_assignable_token = NULL;
-// 	while (tmp)
-// 	{
-// 		parser.current = ((t_token *)tmp->content);
-// 		if (parser.current->type == ARG || (parser.current->type == FILES && parser.context == 2))
-// 		{
-// 			if (!parser.last_command)
-// 			{
-// 				tmp_token = create_token(parser.current->str);
-// 				tmp_token->type = COMMAND;
-// 				tmp_lst = ft_lstnew(tmp_token);
-// 				tmp_lst->next = before;
-// 				before->next = tmp->next;
-// 				free(tmp);
-// 				parser.last_command = tmp_token;
-// 				tmp = before->next;
-// 				if (!last_assignable_token)
-// 					split.tokens = tmp_lst;
-// 				else
-// 					last_assignable_token->next = tmp_lst;
-// 				continue ;
-// 			}
-// 			if (before)
-// 			{
-// 				parser.last_command->arg_count++;
-// 				parser.last_command->args = add_to_array(parser.last_command->args, parser.current->str, parser.last_command->arg_count);
-// 				before->next = tmp->next;
-// 				free (tmp);
-// 				tmp = before->next;
-// 				continue ;
-// 			}
-// 		}
-// 		if (parser.current->type == COMMAND)
-// 		{
-// 			parser.context = 0;
-// 			parser.last_command = parser.current;
-// 		}
-// 		if (parser.current->type == PIPE || parser.current->type == RANDOM)
-// 		{
-// 			last_assignable_token = tmp;
-// 			parser.context = 0;
-// 			parser.last_command = NULL;
-// 		}
-// 		if (parser.current->type == REDIRECTION || parser.current->type == APPEND)
-// 		{
-// 			parser.last_redirect = parser.current;
-// 			parser.context = 1;
-// 		}
-// 		if (parser.current->type == FILES && parser.context == 1)
-// 		{
-// 			// parser.last_redirect->args = malloc(sizeof(char *) * 2);
-// 			// parser.last_redirect->args[0] = parser.current->str;
-// 			// parser.last_redirect->args[1] = NULL;
-// 			// parser.last_redirect->arg_count++;
-// 			parser.last_redirect->args = add_to_array(parser.last_redirect->args, parser.current->str, ++parser.last_redirect->arg_count);
-// 			parser.context = 2;
-// 			before->next = tmp->next;
-// 			free (tmp);
-// 			tmp = before->next;
-// 			continue ;
-// 		}
-// 		before = tmp;
-// 		tmp = tmp->next;
-// 	}
-// 	return (split.tokens);
-// }
+/**
+* 0 tmp
+* 1 before
+* 2 tmp_lst;
+* 3 last_assignable_token
+*/
 
 t_list	*parse_tokens(t_list *tokens)
 {
-	t_list		*tmp;
-	t_list		*before;
 	t_token		*tmp_token;
-	t_list		*tmp_lst;
-	t_list		*last_assignable_token;
 	t_parser	parser;
-	
-	tmp = tokens;
-	before = NULL;
+	t_list		*list[4];
+
+	list[0] = tokens;
+	list[1] = NULL;
 	parser.context = 0;
 	parser.last_command = NULL;
-	last_assignable_token = NULL;
-	while (tmp)
+	list[3] = NULL;
+	while (list[0])
 	{
-		parser.current = ((t_token *)tmp->content);
-		if (parser.current->type == ARG || (parser.current->type == FILES && parser.context == 2))
+		parser.current = ((t_token *)list[0]->content);
+		if (parser.current->type == ARG
+			|| (parser.current->type == FILES && parser.context == 2))
 		{
 			if (!parser.last_command)
 			{
 				tmp_token = create_token(parser.current->str);
 				tmp_token->type = COMMAND;
-				tmp_lst = ft_lstnew(tmp_token);
-				tmp_lst->next = before;
-				before->next = tmp->next;
-				free(tmp);
+				list[2] = ft_lstnew(tmp_token);
+				list[2]->next = list[1];
+				list[1]->next = list[0]->next;
+				free(list[0]);
 				parser.last_command = tmp_token;
-				tmp = before->next;
-				if (!last_assignable_token)
-					tokens = tmp_lst;
+				list[0] = list[1]->next;
+				if (!list[3])
+					tokens = list[2];
 				else
-					last_assignable_token->next = tmp_lst;
+					list[3]->next = list[2];
 				continue ;
 			}
-			if (before)
+			if (list[1])
 			{
 				parser.last_command->arg_count++;
-				parser.last_command->args = add_to_array(parser.last_command->args, parser.current->str, parser.last_command->arg_count);
-				before->next = tmp->next;
-				free (tmp);
-				tmp = before->next;
+				parser.last_command->args = add_to_array(
+						parser.last_command->args, parser.current->str,
+						parser.last_command->arg_count);
+				list[1]->next = list[0]->next;
+				free (list[0]);
+				list[0] = list[1]->next;
 				continue ;
 			}
 		}
@@ -157,26 +71,29 @@ t_list	*parse_tokens(t_list *tokens)
 		}
 		if (parser.current->type == PIPE || parser.current->type == RANDOM)
 		{
-			last_assignable_token = tmp;
+			list[3] = list[0];
 			parser.context = 0;
 			parser.last_command = NULL;
 		}
-		if (parser.current->type == REDIRECTION || parser.current->type == APPEND)
+		if (parser.current->type == REDIRECTION
+			|| parser.current->type == APPEND)
 		{
 			parser.last_redirect = parser.current;
 			parser.context = 1;
 		}
 		if (parser.current->type == FILES && parser.context == 1)
 		{
-			parser.last_redirect->args = add_to_array(parser.last_redirect->args, parser.current->str, ++parser.last_redirect->arg_count);
+			parser.last_redirect->args = add_to_array(
+					parser.last_redirect->args,
+					parser.current->str, ++parser.last_redirect->arg_count);
 			parser.context = 2;
-			before->next = tmp->next;
-			free (tmp);
-			tmp = before->next;
+			list[1]->next = list[0]->next;
+			free (list[0]);
+			list[0] = list[1]->next;
 			continue ;
 		}
-		before = tmp;
-		tmp = tmp->next;
+		list[1] = list[0];
+		list[0] = list[0]->next;
 	}
 	return (tokens);
 }
@@ -185,13 +102,14 @@ t_list	*parser(char	*line)
 {
 	t_spliter	split;
 	t_syntax	syntax;
-	
+
 	split = spliter(line);
 	tokenizer(split.tokens);
 	syntax = syntax_analysis(split.tokens);
 	if (syntax.error)
 	{
-		printf("syntax error m8 code : %d near %s\n", syntax.error, syntax.err_token.str);
+		printf("syntax error m8 code : %d near %s\n",
+			syntax.error, syntax.err_token.str);
 		g_shell.error = 2;
 		return (split.tokens);
 	}
