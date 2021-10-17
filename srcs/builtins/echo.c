57c5664379154fad17709243d544c59e209ea6dc
Oumar Mimouni <omimouni@student.1337.ma>
2021-10-17 10:05:57 +0100

    fixed problem in echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omimouni <omimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 09:35:17 by omimouni          #+#    #+#             */
-/*   Updated: 2021/09/25 10:37:44 by iharchi          ###   ########.fr       */
+/*   Updated: 2021/10/17 10:05:27 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	builtin_echo(t_token command)
 			new_line = 0;
 		}
 		while (command.args[i])
-			printf("%s ", command.args[i++]);
+			printf("%s", command.args[i++]);
 	}
 	if (new_line)
 		printf("\n");
