0c7118691aef10ec1373891d1776ba1a40ad9423
mrmoustach <bossissamos@gmail.com>
2021-10-29 18:01:10 +0100

    fixed norm in execute

--- srcs/builtins/execute.c
+++ srcs/builtins/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omimouni <omimouni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 10:28:32 by iharchi           #+#    #+#             */
-/*   Updated: 2021/10/29 17:21:52 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/10/29 17:58:16 by iharchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,6 @@ int	builtin_pipe_handler(t_token command)
 {
 	int	pid;
 
-	// TODO: Not sure of this init is right
-	pid = 0;
 	if (command.in_pipe)
 	{
 		pid = fork();
