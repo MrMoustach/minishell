788c87e71bf8e02a184ffdad17950ac1e16e7ad9
Oumar Mimouni <omimouni@student.1337.ma>
2021-11-05 10:05:57 +0100

    edited libft

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,27 +6,25 @@
 /*   By: omimouni <omimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 12:33:37 by iharchi           #+#    #+#             */
-/*   Updated: 2021/11/05 09:59:57 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/11/05 10:03:20 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
-// TODO: Test "exit abcd"
-// TODO: Fix number overflow
-// TODO: Make ltoa
-
-int	builtin_exit_overflow(char *str) {
+int	builtin_exit_overflow(char *str)
+{
 	long long	number;
 	char		*tmp;
 	int			flag;
 
+	if (!ft_is_number(str))
+		return (0);
 	flag = 1;
 	number = ft_atol(str);
-	tmp	   = ft_ltoa(number);
+	tmp = ft_ltoa(number);
 	if (strncmp(str, tmp, ft_strlen(str)))
-		flag = 0;	
+		flag = 0;
 	free(tmp);
 	return (flag);
 }
@@ -47,13 +45,13 @@ void	builtin_exit(t_token command)
 	}
 	if (command.arg_count)
 	{
-		if (ft_is_number(command.args[0]) && builtin_exit_overflow(command.args[0]))
+		if (builtin_exit_overflow(command.args[0]))
 			exit_code = (int)ft_atol(command.args[0]);
 		else
 		{
 			g_shell.error = 6;
 			handle_errors(NULL, command.args[0]);
-			exit_code = 255; // FIXED: exit code
+			exit_code = 255;
 		}
 	}
 	free_tab(g_shell.envp);
